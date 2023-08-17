5d38476a6808abe8f9966d53d2c5637649618b16
ronit singh <ronit2001krish@gmail.com>
2023-08-17 21:10:24 +0530

    custom hooks

---
+++ custom-hooks/src/components/NewTask/NewTask.js
@@ -0,0 +1,55 @@
+
+import Section from '../UI/Section';
+import TaskForm from './TaskForm';
+import useHttp from '../../hooks/use-http';
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest } = useHttp();
+
+
+
+  const enterTaskHandler = async (taskText) => {
+
+    sendRequest({ url: 'https://resume-builderr-default-rtdb.firebaseio.com//tasks.json', method: "POST", headers: { "Content-Type": "application/json"}, body: { text: taskText }  }, (data) => {
+      const generatedId = data.name; // firebase-specific => "name" contains generated id
+      const createdTask = { id: generatedId, text: taskText };
+
+      props.onAddTask(createdTask);
+    })
+
+    // setIsLoading(true);
+    // setError(null);
+    // try {
+    //   const response = await fetch(
+    //     'https://resume-builderr-default-rtdb.firebaseio.com//tasks.json',
+    //     {
+    //       method: 'POST',
+    //       body: JSON.stringify({ text: taskText }),
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //     }
+    //   );
+
+    //   if (!response.ok) {
+    //     throw new Error('Request failed!');
+    //   }
+
+    //   const data = await response.json();
+
+
+    // } catch (err) {
+    //   setError(err.message || 'Something went wrong!');
+    // }
+    // setIsLoading(false);
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
